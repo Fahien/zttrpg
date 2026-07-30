7330cb9e4458ded7960bab75eb20b4f0133481a7
Antonio Caggiano <info@antoniocaggiano.eu>
2026-07-31 01:17:39 +0200

    db: Helper struct

--- src/migration.zig
+++ src/migration.zig
@@ -8,39 +8,36 @@ const pq = @import("pq.zig");
 
 /// Run all sql files in the `db` directory.
 pub fn main(init: std.process.Init) !void {
-    const conn = pq.PQconnectdb("dbname=zttrpg") orelse return error.ConnectionFailed;
-    defer pq.PQfinish(conn);
-    if (pq.PQstatus(conn) != pq.PGStatus.CONNECTION_OK) {
-        const err = pq.PQerrorMessage(conn);
-        std.debug.print("Connection failed: {s}\n", .{err});
+    const conn = try pq.Connection.connect("dbname=zttrpg");
+    defer conn.close();
+    if (conn.status() != .CONNECTION_OK) {
+        std.debug.print("Connection failed: {s}\n", .{conn.errorMessage()});
         return error.ConnectionFailed;
     }
 
     // Create schema_migrations if it doesn't exist
     const create_schema_migrations_sql = "CREATE TABLE IF NOT EXISTS schema_migrations (version TEXT PRIMARY KEY)";
-    const schema_migration_result = pq.PQexec(conn, create_schema_migrations_sql) orelse return error.ExecutionFailed;
-    defer pq.PQclear(schema_migration_result);
-    if (pq.PQresultStatus(schema_migration_result) != pq.PGExecStatusType.PGRES_COMMAND_OK) {
-        const err = pq.PQerrorMessage(conn);
-        std.debug.print("Execution failed: {s}\n", .{err});
+    const schema_migration_result = try conn.exec(create_schema_migrations_sql);
+    defer schema_migration_result.deinit();
+    if (schema_migration_result.status() != .PGRES_COMMAND_OK) {
+        std.debug.print("Execution failed: {s}\n", .{conn.errorMessage()});
         return error.ExecutionFailed;
     }
 
     // Get list of already applied migrations.
     const applied_migrations_sql = "SELECT version FROM schema_migrations";
-    const applied_migrations_result = pq.PQexec(conn, applied_migrations_sql) orelse return error.ExecutionFailed;
-    defer pq.PQclear(applied_migrations_result);
-    if (pq.PQresultStatus(applied_migrations_result) != pq.PGExecStatusType.PGRES_TUPLES_OK) {
-        const err = pq.PQerrorMessage(conn);
-        std.debug.print("Execution failed: {s}\n", .{err});
+    const applied_migrations_result = try conn.exec(applied_migrations_sql);
+    defer applied_migrations_result.deinit();
+    if (applied_migrations_result.status() != .PGRES_TUPLES_OK) {
+        std.debug.print("Execution failed: {s}\n", .{conn.errorMessage()});
         return error.ExecutionFailed;
     }
 
     var applied_migrations = std.StringHashMap(void).init(init.gpa);
     defer applied_migrations.deinit();
-    const tuple_count = pq.PQntuples(applied_migrations_result);
+    const tuple_count = applied_migrations_result.len();
     for (0..@intCast(tuple_count)) |i| {
-        const version_cstr = pq.PQgetvalue(applied_migrations_result, @intCast(i), 0);
+        const version_cstr = applied_migrations_result.getValue(@intCast(i), 0);
         const version_str = std.mem.span(version_cstr);
 
         try applied_migrations.put(version_str, {});
@@ -65,24 +62,22 @@ pub fn main(init: std.process.Init) !void {
         const sql_cstr = try init.gpa.dupeZ(u8, sql_str);
         defer init.gpa.free(sql_cstr);
 
-        const result = pq.PQexec(conn, sql_cstr) orelse return error.ExecutionFailed;
-        defer pq.PQclear(result);
+        const result = try conn.exec(sql_cstr);
+        defer result.deinit();
 
-        const status = pq.PQresultStatus(result);
-        if (status != pq.PGExecStatusType.PGRES_COMMAND_OK and status != pq.PGExecStatusType.PGRES_TUPLES_OK) {
-            const err = pq.PQerrorMessage(conn);
-            std.debug.print("Execution failed: {s}\n", .{err});
+        const status = result.status();
+        if (status != .PGRES_COMMAND_OK and status != .PGRES_TUPLES_OK) {
+            std.debug.print("Execution failed: {s}\n", .{conn.errorMessage()});
             return error.ExecutionFailed;
         }
 
         const insert_migration_sql = "INSERT INTO schema_migrations (version) VALUES ('{s}')";
         const insert_migration_sql_fmt = try std.fmt.allocPrintSentinel(init.gpa, insert_migration_sql, .{name}, 0);
         defer init.gpa.free(insert_migration_sql_fmt);
-        const insert_result = pq.PQexec(conn, insert_migration_sql_fmt) orelse return error.ExecutionFailed;
-        defer pq.PQclear(insert_result);
-        if (pq.PQresultStatus(insert_result) != pq.PGExecStatusType.PGRES_COMMAND_OK) {
-            const err = pq.PQerrorMessage(conn);
-            std.debug.print("Execution failed: {s}\n", .{err});
+        const insert_result = try conn.exec(insert_migration_sql_fmt);
+        defer insert_result.deinit();
+        if (insert_result.status() != .PGRES_COMMAND_OK) {
+            std.debug.print("Execution failed: {s}\n", .{conn.errorMessage()});
             return error.ExecutionFailed;
         }
     }
--- src/pq.zig
+++ src/pq.zig
@@ -1,31 +1,96 @@
 // © 2026 Antonio Caggiano
 // SPDX-License-Identifier: MIT
 
-pub const PGconn = opaque {};
-pub extern fn PQconnectdb(conninfo: [*]const u8) ?*PGconn;
+const std = @import("std");
+
+const PGconn = opaque {};
+extern fn PQconnectdb(conninfo: [*]const u8) ?*PGconn;
 
 pub const PGStatus = enum(u32) {
     CONNECTION_OK = 0,
     CONNECTION_BAD = 1,
 };
-pub extern fn PQstatus(conn: *PGconn) PGStatus;
+extern fn PQstatus(conn: *PGconn) PGStatus;
 
-pub extern fn PQerrorMessage(conn: *PGconn) [*:0]const u8;
+extern fn PQerrorMessage(conn: *PGconn) [*:0]const u8;
 
-pub extern fn PQfinish(conn: *PGconn) void;
+extern fn PQfinish(conn: *PGconn) void;
 
-pub const PGresult = opaque {};
-pub extern fn PQexec(conn: *PGconn, query: [*:0]const u8) ?*PGresult;
+const PGresult = opaque {};
+extern fn PQexec(conn: *PGconn, query: [*:0]const u8) ?*PGresult;
 
 pub const PGExecStatusType = enum(u32) {
     PGRES_COMMAND_OK = 1,
     PGRES_TUPLES_OK = 2,
     // >=3 trouble
 };
-pub extern fn PQresultStatus(res: *PGresult) PGExecStatusType;
+extern fn PQresultStatus(res: *PGresult) PGExecStatusType;
 
-pub extern fn PQclear(res: *PGresult) void;
+extern fn PQclear(res: *PGresult) void;
 
-pub extern fn PQntuples(res: *PGresult) c_int;
+extern fn PQntuples(res: *PGresult) c_int;
 
-pub extern fn PQgetvalue(res: *PGresult, row: c_int, col: c_int) [*:0]const u8;
+extern fn PQgetvalue(res: *PGresult, row: c_int, col: c_int) [*:0]const u8;
+
+pub const Connection = struct {
+    conn: *PGconn,
+
+    pub fn connect(conninfo: [*]const u8) !Connection {
+        const conn = PQconnectdb(conninfo) orelse return error.ConnectionFailed;
+        if (PQstatus(conn) != PGStatus.CONNECTION_OK) {
+            const err = PQerrorMessage(conn);
+            std.debug.print("Connection failed: {s}\n", .{err});
+            return error.ConnectionFailed;
+        }
+        return Connection{ .conn = conn };
+    }
+
+    pub fn status(self: *const Connection) PGStatus {
+        return PQstatus(self.conn);
+    }
+
+    pub fn errorMessage(self: *const Connection) [*:0]const u8 {
+        return PQerrorMessage(self.conn);
+    }
+
+    pub fn exec(self: *const Connection, query: [*:0]const u8) !Result {
+        const res = PQexec(self.conn, query) orelse return error.ExecutionFailed;
+        if (PQresultStatus(res) != PGExecStatusType.PGRES_COMMAND_OK and
+            PQresultStatus(res) != PGExecStatusType.PGRES_TUPLES_OK)
+        {
+            const err = PQerrorMessage(self.conn);
+            std.debug.print("Execution failed: {s}\n", .{err});
+            PQclear(res);
+            return error.ExecutionFailed;
+        }
+        return Result.init(res);
+    }
+
+    pub fn close(self: *const Connection) void {
+        PQfinish(self.conn);
+    }
+};
+
+pub const Result = struct {
+    res: *PGresult,
+
+    fn init(self: *PGresult) Result {
+        return Result{ .res = self };
+    }
+
+    pub fn deinit(self: *const Result) void {
+        PQclear(self.res);
+    }
+
+    pub fn status(self: *const Result) PGExecStatusType {
+        return PQresultStatus(self.res);
+    }
+
+    pub fn len(self: *const Result) c_int {
+        return PQntuples(self.res);
+    }
+
+    pub fn getValue(self: *const Result, row: c_int, col: c_int) [*:0]const u8 {
+        return PQgetvalue(self.res, row, col);
+    }
+};
