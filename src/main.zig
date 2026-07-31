53a4785d1b46838ddae18a887b61b8bf87deda75
Antonio Caggiano <info@antoniocaggiano.eu>
2026-07-31 14:15:12 +0200

    feat: TCP skeleton

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,31 @@
 // SPDX-License-Identifier: MIT
 
 const std = @import("std");
-
 const zttrpg = @import("zttrpg");
 const pq = @import("pq");
+
+const Io = std.Io;
 
 pub fn main(init: std.process.Init) !void {
     // Prints to stderr, unbuffered, ignoring potential errors.
     std.debug.print("ZTTRPG\n", .{});
 
+    // TCP skeleton.
+    const address = "127.0.0.1";
+    const port = 8080;
+
+    const ip_address = try Io.net.IpAddress.parse(address, port);
+    var server = try ip_address.listen(init.io, .{ .mode = .stream });
+    defer server.deinit(init.io);
+
+    while (true) {
+        const conn = try server.accept(init.io);
+        defer conn.close(init.io);
+        std.debug.print("Accepted connection from {s}:{d}\n", .{ conn.socket.address.ip4.bytes, conn.socket.address.getPort() });
+    }
+}
+
+fn readCharacters(init: std.process.Init) !void {
     const db = try zttrpg.Database.init();
     defer db.deinit();
 
