f3a6ee79433e5f18dc1a256209a2be343ce356e8
Antonio Caggiano <info@antoniocaggiano.eu>
2026-07-31 16:46:11 +0200

    db: Check SQL

--- db/0001-characters.sql
+++ db/0001-characters.sql
@@ -1,7 +1,9 @@
 CREATE TABLE characters (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
-    level INTEGER NOT NULL
+    level INTEGER NOT NULL,
+    CHECK (level >= 1 AND level <= 100),
+    CHECK (name <> '')
 );
 
 INSERT INTO characters (name, level) VALUES
